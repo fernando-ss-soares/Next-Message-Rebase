1816332f0778c869bfaa05a6567a453877e404c3
Fernando Soares <fernando.git898@gmail.com>
2023-02-18 13:47:32 -0300

    :sparkles: feat: Página de Pesquisa de Empresas

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,8 +1,12 @@
 import Title from "@/src/components/utils/header/title";
 import Menu from "@/src/components/settings/menu";
 import Footer from "@/src/components/settings/footer";
+import { parseCookies } from "nookies";
 
 export default function Settings() {
+  
+  const cookies = parseCookies();
+  
   return (
     <>
       <div className="container px-4 py-5">
@@ -11,7 +15,7 @@ export default function Settings() {
 
       <div className="container px-4 py-5">
         <div className="my-3 p-3 bg-body rounded shadow-sm">
-          <Menu title="All settings" />
+          <Menu title="All settings" cookies={cookies} />
 
           <Footer title="Next Message" />
         </div>
